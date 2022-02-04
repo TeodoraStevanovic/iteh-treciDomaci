e3bab3b57a6f3d8d7119773cb4dff0e35b4feaa2
Teodora Stevanovic <teodorastevanovic26@gmail.com>
2022-02-04 18:30:06 +0100

    kreirana druga stranica

---
+++ src/components/Like.jsx
@@ -0,0 +1,14 @@
+import OneCar from "./OneCar";
+
+const Like = ({ cars }) => {
+    return (
+      <div className="liked-container">
+     {/* {cars.map((c) => (
+          <OneCar car={c} key={c.id} inCart={0} />
+        ))}
+     */ }  
+      </div>
+    );
+  };
+  
+  export default Like;
